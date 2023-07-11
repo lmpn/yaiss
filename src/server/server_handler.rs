fa038e3be3de07c237a68295a1790a5fe872f95d
Luís Neto <luis.neto.dev@gmail.com>
2023-07-11 18:46:21 +0100

    #7: create workflows
    - implemented a start and stop test for the server
    - added reqwest as dev dependency
    - simple workflow with build tests and static analysis with clippy

--- src/server/server_handler.rs
+++ src/server/server_handler.rs
@@ -53,3 +53,30 @@ impl ServerHandler {
 async fn get_home() -> &'static str {
     "Hello world"
 }
+
+#[cfg(test)]
+mod tests {
+    use tokio::time::sleep;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn start_and_stop() {
+        let mut sh = ServerHandler::new();
+        sh.serve();
+        sleep(Duration::from_millis(500)).await;
+        let response = reqwest::get("http://0.0.0.0:3000/")
+            .await
+            .expect("failed to perfrom GET /")
+            .text()
+            .await
+            .expect("failed to read payload");
+        assert_eq!(response, "Hello world");
+        sh.stop().await;
+
+        let response = reqwest::get("http://0.0.0.0:3000/")
+            .await
+            .expect_err("expected error");
+        assert!(response.is_request());
+    }
+}
