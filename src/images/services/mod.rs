6884147425312c7919d2c1435da2d21c58ff4095
Luis Neto <luis.neto.dev@gmail.com>
2023-08-06 17:33:07 +0100

    update mod.rs files and add batch delete route

--- src/images/services/mod.rs
+++ src/images/services/mod.rs
@@ -1,1 +1,2 @@
 pub mod upload_images_service;
+pub mod delete_images_service;
--- src/images/web/mod.rs
+++ src/images/web/mod.rs
@@ -2,10 +2,13 @@ use axum::{body::Body, routing::post, Router};
 
 use crate::state::State;
 
+pub mod delete_images_handler;
 pub mod upload_images_handler;
 
 pub fn router() -> Router<State, Body> {
-    let images_routes = Router::new().route("/", post(upload_images_handler::upload_image));
+    let images_routes = Router::new()
+        .route("/", post(upload_images_handler::upload_image))
+        .route("/batch_delete", post(delete_images_handler::delete_images));
     let images_router = Router::new().nest("/images", images_routes);
     Router::new().nest("/api/v1", images_router)
 }
