0ef679d995b949a0e11a225a9198ea1680d8618e
Luis Neto <luis.neto.dev@gmail.com>
2023-08-22 21:48:53 +0100

    add tests for delete_image_handler

--- src/web/images/delete_image_handler.rs
+++ src/web/images/delete_image_handler.rs
@@ -47,4 +47,112 @@ pub async fn delete_image_handler(
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+
+    use std::sync::Arc;
+
+    use async_trait::async_trait;
+    use axum::{body::Body, http::Request, routing::delete, Router};
+    use mockall::{mock, predicate};
+    use reqwest::StatusCode;
+    use serde_json::{json, Value};
+
+    use crate::{
+        services::images::ports::incoming::delete_image_service::{
+            DeleteImageService, DeleteImageServiceError,
+        },
+        web::images::delete_image_handler,
+    };
+    use tower::ServiceExt; // for `oneshot` and `ready`
+
+    mock! {
+        pub Service {}
+        #[async_trait]
+        impl DeleteImageService for Service {
+            async fn delete_image(&self, index: i64) -> Result<(), DeleteImageServiceError>;
+        }
+    }
+
+    pub fn app(service: MockService) -> Router<(), Body> {
+        let delete_image_service =
+            Arc::new(service) as delete_image_handler::DynDeleteImagesService;
+        Router::new()
+            .route(
+                "/:identifier",
+                delete(delete_image_handler::delete_image_handler),
+            )
+            .with_state(delete_image_service)
+    }
+
+    #[tokio::test]
+    async fn on_image_deleted_return_ok() {
+        let mut mock_service = MockService::new();
+        mock_service
+            .expect_delete_image()
+            .with(predicate::eq(1))
+            .returning(move |_i| Ok(()));
+        let app = app(mock_service);
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .method("DELETE")
+                    .uri("/1")
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        assert!(body.is_empty());
+    }
+
+    #[tokio::test]
+    async fn on_internal_error_return_internal_server_code() {
+        let mut mock_service = MockService::new();
+        mock_service
+            .expect_delete_image()
+            .returning(move |_i| Err(DeleteImageServiceError::InternalError));
+        let app = app(mock_service);
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .method("DELETE")
+                    .uri("/1")
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::INTERNAL_SERVER_ERROR);
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        let body: Value = serde_json::from_slice(&body).unwrap();
+        assert_eq!(body, json!({"error": "internal error deleting image"}));
+    }
+
+    #[tokio::test]
+    async fn on_image_not_found_return_not_found_code() {
+        let mut mock_service = MockService::new();
+        mock_service
+            .expect_delete_image()
+            .returning(move |_i| Err(DeleteImageServiceError::ImageNotFound));
+        let app = app(mock_service);
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .method("DELETE")
+                    .uri("/1")
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::NOT_FOUND);
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        let body: Value = serde_json::from_slice(&body).unwrap();
+        assert_eq!(body, json!({ "error": "image not found", }));
+    }
+}
